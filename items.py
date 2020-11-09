e20fd5ac91d201cc119047497dffb28227415c6f
Alperen Yurtseven <31616211+alpyurtseven@users.noreply.github.com>
2020-11-09 16:38:20 +0300

    Update items.py

--- items.py
+++ items.py
@@ -1,5 +1,5 @@
 class Item:
     def __init__(self,value,weight):
-        self.value=value   #paketin değeri
-        self.weight=weight #paketin ağırlığı
+        self.value=value   #package value
+        self.weight=weight #package weight
     
